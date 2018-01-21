1a94ade7b1893e450604aa0904557470a43d5735
Jason Kao <jasonkao85@gmail.com>
2018-01-21 05:01:02 -0500

    states

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -11,11 +11,14 @@ def parse_tables(content):
     for table in wikitables[:-1]:
         analyze_table(table)
 
-
 def analyze_table(table):
     info = {}
     for tr in table.findAll('tr')[2:]:
-        print tr.find('th').find('a').string.encode('utf-8').replace('at-large', '').replace("\xc2\xa0", '')
+        code = tr.find('th').find('a').string.encode('utf-8').replace('at-large', '').replace("\xc2\xa0", '')
+        results = str(tr.findAll('td')[-1])
+        rep = results[results.find('(Republican)') + 13:results.find('(Republican)') + 17]
+        dem = results[results.find('(Democratic)') + 13:results.find('(Democratic)') + 17]
+        print rep, dem
 
 if __name__ == '__main__':
     with open('wiki.html') as f:
